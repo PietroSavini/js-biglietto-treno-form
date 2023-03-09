373d91d20aac5f04692198eb0d1e9869793148e8
PietroSavini <savinipietro@gmail.com>
2023-03-09 19:08:27 +0100

    END OF PROJECT

--- js/script.js
+++ js/script.js
@@ -1,58 +1,72 @@
 //costanti
-const priceKm = (0.21)
-const minorDiscount = (0.80);
-const elderDiscount = (0.60);
+let kmPrice = 0.21;
+console.log(kmPrice);
+let username;
+let kmTravel;
+let ageDiscount;
+let finalPrice;
+let offerType;
+let rN1;
+let rN2;
 
 //INPUT ------------------->variable initializzation from user
+const submitBtn = document.getElementById("submit");
 
-const submitBtn = document.getElementById('submit');
 submitBtn.addEventListener("click", function() {
-    const usernameInput = document.getElementById('user-id');
-    const username = usernameInput.value;
+    //input initializzation
+    //data control for username
+    const regex = /^[a-zA-Z\s]+$/;
+    const usernameInput = document.getElementById("user-id");
+    let username = usernameInput.value;
+    if (!regex.test(username)){
+
+        alert ('Il nome può contenere solo lettere e spazi, la pagina verrà ricaricata');
+        location.reload();
+
+    }else {
+
+        const kmInput = document.getElementById('km-travel');
+        kmTravel = kmInput.value;
+
+        //age discount initializzation
+        const ageInput = document.getElementById('user-age')
+        console.log(ageInput.value);
+
+        if (ageInput.value === 'minorenne'){
+            ageDiscount = ('0.80');
+            offerType = ('Sconto Minorenni');
+
+        }else if (ageInput.value === 'over-65'){
+            ageDiscount = ('0.60');
+            offerType = ('Sconto Over-65');
+        }else{
+            ageDiscount = ('1');
+            offerType = ('Biglietto standard');
+        }
+        console.log(offerType);
+    }
+    
+    //OUTPUT ON SCREEN----------------------------------------------
+    //calculations for final price
+    ageDiscount = parseFloat(ageDiscount);
+    kmTravel = parseFloat(kmTravel);
+    kmPrice = parseFloat(kmPrice);
+    finalPrice = (kmTravel * kmPrice) * ageDiscount;
+    finalPrice = finalPrice.toFixed(2);
+    document.getElementById("final-price").innerHTML = '€' + `${finalPrice}`;
     console.log(username);
-}
+    console.log(kmTravel);
+    console.log(ageDiscount);
+    
+    //offet Type display
+    document.getElementById("ticket-typeof").innerHTML = offerType;
+
+    // random number generators
+    rN1 = Math.floor(Math.random() *100000);
+    rN2 = Math.floor(Math.random() *10 + 1);
+    document.getElementById('cp-code').innerHTML = rN1;
+    document.getElementById('wagon-num').innerHTML = rN2;
+    document.getElementById('user').innerHTML = username;
+}   
 )
 
-// let userName = document.getElementById('user-id');
-// let age;
-// let totalKm;
-// let finalPrice;
-
-// //data control for userName
-// const regex = /^[a-zA-Z]+$/;
-
-// if (!regex.test(userName)){
-//     alert ('Il nome può contenere solo lettere');
-//     location.reload();
-// }else {
-//      age = prompt('inserisca la sua età');
-//     //data control
-//     if(age > 150){
-//         alert('non accettiamo mummie o salme a bordo');
-//         location.reload();
-//     }else{
-//          totalKm = prompt('per quanti km deve viaggiare a bordo del nostro treno? (inserisca numeri interi)');
-//     } 
-// }
-
-// //variables as numbers
-// age = parseInt(age);
-// totalKm = parseInt(totalKm);
-// finalPrice = parseInt(finalPrice);
-
-// console.log(userName);
-// console.log(age);
-// console.log(totalKm);
-
-// //discount calculations
-// if(age < 18){
-//     finalPrice = (totalKm * priceKm) * minorDiscount;
-// }else if(age >= 65){
-//     finalPrice = (totalKm * priceKm) * elderDiscount;
-// }else{
-//     finalPrice = totalKm * priceKm;
-// }
-
-// //final price with 2 decimals
-// finalPrice = finalPrice.toFixed(2)
-// console.log('this is the final price:' + finalPrice);
